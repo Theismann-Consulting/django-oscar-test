00c0b5745dc23bb32ec03124f6863c707b461ab0
Andrew Theismann <andrew@theismannconsulting.com>
2024-08-22 16:08:21 -0400

    revert to sqllite for default

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -18,7 +18,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Use a Sqlite database by default
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DATABASE_ENGINE', 'django.db.backends.postgresql'),
+        'ENGINE': os.environ.get('DATABASE_ENGINE', 'django.db.backends.sqlite3'),
         'NAME': os.environ.get('DATABASE_NAME', location('db.sqlite')),
         'USER': os.environ.get('DATABASE_USER', None),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD', None),
